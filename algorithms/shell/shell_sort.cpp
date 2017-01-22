b919b242cc530f7a6302d3253e0eb8acfdd6e7ab
Arden <ardenisthebest@gmail.com>
2017-01-22 14:17:10 -0800

    Updated shell

--- algorithms/shell/shell_sort.cpp
+++ algorithms/shell/shell_sort.cpp
@@ -1,5 +1,6 @@
 #include "../../sort_headers.hpp"
 #include <appareo.h>
+#include <pessum.h>
 #include <cmath>
 #include <ctime>
 #include <iostream>
@@ -14,27 +15,42 @@ double sort::ShellSort(bool display) {
   std::vector<int> gaps;
   int gap = (values.size() / (pow(2, 1)));
   int counter = 1;
-  while (gap <= 1) {
+  pessum::logging::Log(pessum::logging::DATA, std::to_string(gap));
+  while (gap > 1) {
     gaps.push_back(gap);
     counter++;
     gap = (values.size() / (pow(2, counter)));
   }
-  if (gap > 1) {
+  if (gap <= 1) {
     gaps.push_back(1);
   }
+  std::string line = "";
+  for(int i = 0; i <values.size();i++){
+	  line += std::to_string(values[i]) + ",";
+  }
+  pessum::logging::Log(pessum::logging::INFORMATION, line);
+  line = "";
+  for(int i =0;i<gaps.size();i++){
+	line += std::to_string(gaps[i]) + ",";
+  }
+  pessum::logging::Log(pessum::logging::DATA, line);
   for (int i = 0; i < gaps.size(); i++) {
-    for (int j = gaps[i]; j < values.size(); j++) {
-      int k;
-      for (k = j; k >= gaps[i] && values[k - gaps[i]] > values[j];
-           k -= gaps[i]) {
-        values[k] = values[k - gaps[i]];
+    for (int j = 0; j < values.size(); j++) {
+      if(j + gaps[i] < values.size() && values[j + gaps[i]] < values[j]){
+      	iter_swap(values.begin() + j + gaps[i], values.begin() + j);
+      } else if(j - gaps[i] >= 0 && values[j - gaps[i]] > values[j]){
+	      iter_swap(values.begin() + j - gaps[i], values.begin() + j);
       }
-      values[k] = values[j];
     }
   }
   timeelapsed = appareo::induco::Timer();
   if (display == true) {
     appareo::induco::TerminateProgressBar();
   }
+  line = "";
+  for(int i =0;i<values.size();i++){
+	line += std::to_string(values[i]) + ",";
+  }
+  pessum::logging::Log(pessum::logging::INFORMATION, line);
   return (timeelapsed);
 }
