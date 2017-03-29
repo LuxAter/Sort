ebc509da9e4c9b9ac4534188c9d373c10f87b532
Arden <ardenisthebest@gmail.com>
2017-03-29 10:45:05 -0700

    Added documentation for binary tree sort, and bubble sort

--- algorithms/cycle/cycle.cpp
+++ algorithms/cycle/cycle.cpp
@@ -9,6 +9,7 @@
 void sort::CycleSort() {
   clock_t start = clock();
   for (int i = 0; i < data.size(); i++) {
+    result.vec_access++;
     CycleInsert(data[i], i);
   }
   result.time_elapsed = (double)(clock() - start) / CLOCKS_PER_SEC;
@@ -17,6 +18,8 @@ void sort::CycleSort() {
 void sort::CycleInsert(int value, int cycle_start) {
   int pos = 0;
   for (int i = 0; i < data_base.size(); i++) {
+    result.vec_access++;
+    result.comparisons++;
     if (data_base[i] < value) {
       pos++;
     }
@@ -25,6 +28,7 @@ void sort::CycleInsert(int value, int cycle_start) {
     while (data[pos] == value && pos < data.size() && pos != cycle_start) {
       pos++;
     }
+    result.vec_access+=2;
     int temp = data[pos];
     data[pos] = value;
     value = temp;
@@ -32,13 +36,20 @@ void sort::CycleInsert(int value, int cycle_start) {
   while (pos != cycle_start) {
     pos = 0;
     for (int i = 0; i < data_base.size(); i++) {
+      result.vec_access++;
+      result.comparisons++;
       if (data_base[i] < value) {
         pos++;
       }
     }
+    result.vec_access++;
+    result.comparisons++;
     while (data[pos] == value && pos < data.size()) {
       pos++;
+      result.vec_access++;
+      result.comparisons++;
     }
+    result.vec_access+=2;
     int temp = data[pos];
     data[pos] = value;
     value = temp;
